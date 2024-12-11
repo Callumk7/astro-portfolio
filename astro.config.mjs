bb61afdc5d7414a3366c10e23f34d8d3e4456b2e
Callum Kloos <Callumkloos@gmail.com>
2024-12-11 15:55:30 +0000

    getting there

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
+    site: "https://callumkloos.dev",
     integrations: [mdx(), sitemap()],
     experimental: {
         svg: true,
+    },
+    vite: {
+        plugins: [tsconfigPaths()],
     },
 });
